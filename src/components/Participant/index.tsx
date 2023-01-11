f06c24286d6d6b2bc4ccb472b6a058036ab5a4b6
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-01-10 22:49:09 -0300

    :sparkles:feat: add component participant

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,11 +1,15 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
-export function Participant(){
+type Props = {
+  name:string,
+  onRemove:()=>void;
+}
+export function Participant({name,onRemove}:Props){
   return(
     <View style={styles.container}>
-    <Text  style={styles.name}>Wesley</Text>
-    <TouchableOpacity style={styles.button} >
+    <Text  style={styles.name}>{name}</Text>
+    <TouchableOpacity style={styles.button} onPress={onRemove}>
       <Text style={styles.buttonText}>-</Text>
     </TouchableOpacity>
   </View>
